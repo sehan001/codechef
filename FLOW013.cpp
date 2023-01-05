1344755282585d033fb66ebc6b4cd41239f53291
Mohammed Sehan <2000030708@kluniversity.in>
2023-01-05 12:32:57 +0530

    Create FLOW013.cpp

---
+++ FLOW013.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a,b,c;
+        if(a+b+c==180)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
