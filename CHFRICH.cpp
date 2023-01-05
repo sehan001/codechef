0155a0822d60d7b40df34aff798a35c47406877d
Mohammed Sehan <2000030708@kluniversity.in>
2023-01-05 14:48:41 +0530

    Create CHFRICH.cpp

---
+++ CHFRICH.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<(b-a)/c;
+        cout<<"\n";
+    }
+	return 0;
+}
