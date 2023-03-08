b11192d00f68bc3404ed92cb0913d620bab06168
Mohammed Sehan <2000030708@kluniversity.in>
2023-03-08 10:08:41 +0530

    Create GDTURN.cpp

---
+++ GDTURN.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a+b>6)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
